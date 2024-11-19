9a0132d6fdf4169c6721daafae657d8c51e9c9c2
Sanchit Mishra <sanchit1417@gmail.com>
2024-11-19 19:21:53 +0530

    adding return type and type inference

--- src/a.ts
+++ src/a.ts
@@ -5,3 +5,20 @@ helloWorld("Sanchit");
 function helloWorld( name : string){
     console.log("Hello good afternoon, "+name);
 }
+
+/** this is how you give function a return type 
+ * in this case even if we dont give return type as we are adding 2 numbers then it infers the type (type inference)
+*/
+const sum : number = getSum(1,2);
+console.log(sum);
+
+function getSum( num1 : number , num2 : number) : number{
+    return num1 + num2;
+}
+
+/** this is a example of type inference as without return type */
+const isOver18 : boolean = isLegal(18);
+
+function isLegal( age : number){
+    return age>=18;
+}
