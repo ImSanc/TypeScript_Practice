51b59b3a5b2b6c399a81ad13e40ba872d3c7da21
Sanchit Mishra <sanchit1417@gmail.com>
2024-11-20 20:09:20 +0530

    learning interfaces

---
+++ src/a3.ts
@@ -0,0 +1,25 @@
+interface Person {
+    name : string;
+    age : number;
+    job ?: string;
+    greet( greating : string) : void;
+}
+
+class CEO implements Person{
+    name : string;
+    age: number;
+    job?: string;
+
+    constructor(name : string , age : number, job ?: string ) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet(greating: string): void {
+        console.log(greating+ ` ${this.name}`);
+    }
+}
+
+const Person = new CEO("sanchit",24);
+
+Person.greet("Good morning");
