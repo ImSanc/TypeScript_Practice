eb25603a315f804b879e2c1fd21baceaec50925c
Sanchit Mishra <sanchit1417@gmail.com>
2024-11-23 18:41:25 +0530

    trying enums in ts

---
+++ src/enum.ts
@@ -0,0 +1,14 @@
+enum Direction{
+    Down  = "down",
+    Up = "up",
+    Right = "right",
+    Left = "left"
+}
+
+ function doSomething1(direction : Direction){
+
+    if(direction == Direction.Up){
+        //Do something
+    }
+}
+console.log(Direction.Up);
